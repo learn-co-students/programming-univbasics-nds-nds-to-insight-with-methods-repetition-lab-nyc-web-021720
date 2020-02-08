475063bb3130b5f5c504a119a1f7e27663e7b9cc
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-08 16:20:31 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,9 @@ def directors_totals(source)
     director = source[director_index]
     result[director[:name]] = gross_for_director(director)
     director_index += 1
+    # binding.pry
   end
+
   result
 end
 
@@ -25,19 +27,35 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  list_of_directors = []
+  name_index = 0
+
+  while name_index < source.length do
+    director_name = source[name_index][:name]
+    list_of_directors << director_name
+    name_index += 1
+  end
+
+  list_of_directors
 end
 
 def total_gross(source)
-  # Write this implementation
-  #
-  # Should use methods:
-  # 1. directors_totals: returns a Hash of { dir_name => gross }
-  # 2. list_of_directors: names provides an Array of directors names (use
-  #
-  # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
-  # return the total
+  name_index = 0
+  total_gross = 0
+  
+  while name_index < source.length do
+    total_gross += gross_for_director(source[name_index])
+    name_index += 1
+  end
+
+  total_gross
 end
-
-
+# Write this implementation
+#
+# Should use methods:
+# 1. directors_totals: returns a Hash of { dir_name => gross }
+# 2. list_of_directors: names provides an Array of directors names (use
+#
+# Visit each key (i.e. director name), look up the value in the hash
+# returned by directors_totals, and add it to a running total. When done,
+# return the total
